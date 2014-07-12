a2f53d20c047ecd4fffbc12b1bab447cd690d169
Artem Yankov <artem@badgeville.com>
2014-07-11 20:40:29 -0700

    add comment

--- plot1.R
+++ plot1.R
@@ -1,6 +1,8 @@
 data <- read.csv("household_power_consumption.txt", sep=";", na.strings="?")
 df = data[data$Date == "1/2/2007" | data$Date == "2/2/2007", ]
 
+# Will write plot directly to png file, plot will not
+# show up if you run this in R console, but you will see the png file.
 png(file="plot1.png", bg="white")
 
 hist(df$Global_active_power, col="red", bg="white", xlab="Global Active Power (kilowatts)", main="Global Active Power")
